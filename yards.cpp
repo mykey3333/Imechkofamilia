74820db4c2044e550e9894bc0354e5a628ae3f9b
mykey3333 <32564306+mykey3333@users.noreply.github.com>
2017-10-06 10:31:23 +0300

    Add files via upload

---
+++ yards.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()   
+
+{
+  using namespace std; 
+  int x, farlong = 220;
+   cout<<"Enter the distance in furlongs and convert it to yards:";
+   cout << endl;
+   cin >> x;
+   cout << x <<" farlong = " << farlong * x <<" yards" ;
+   return 0;
+ }
